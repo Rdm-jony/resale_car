f1ba7015310f7bf5e03c4c6a55717518538d24ce
Rdm-jony <jonydascse@gmail.com>
2022-11-09 07:32:50 -0800

    edit-my-reviewsection

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
         <div className='img-overlay relative block'>
             <img className='w-full h-screen' src={img} alt=''></img>
             <div className='absolute top-1/3 z-10 w-full'>
-                <h1 class="text-6xl text-center text-slate-300">Enjoy a Luxury Experience</h1>
+                <h1 className="text-6xl text-center text-slate-300">Enjoy a Luxury Experience</h1>
                 <p className='w-2/3 mx-auto my-7 text-lg'>Welcome to the best five-star deluxe hotel in New York. Hotel elementum sesue the aucan vestibulum aliquam justo in sapien rutrum volutpat. Donec in quis the pellentesque velit. Donec id velit ac arcu posuere blane</p>
                 <div className='flex'>
                     <button className="btn btn-outline btn-accent  mx-auto">Services <AiOutlineArrowRight></AiOutlineArrowRight></button>
